53b3bc32b596bf0aefc3436f3187384ecae0e020
Felix Schulze <code@felixschulze.de>
2011-11-14 12:42:22 +0100

    Format test error message for teamcity

--- xcode-teamcity-plugin-agent/src/main/java/de/felixschulze/teamcity/xcode/tools/XCodeBuildOutputParser.java
+++ xcode-teamcity-plugin-agent/src/main/java/de/felixschulze/teamcity/xcode/tools/XCodeBuildOutputParser.java
@@ -46,42 +46,53 @@ public class XCodeBuildOutputParser {
         this.ignoreUnitTests = ignoreUnitTests;
     }
 
+    private String escapeForTeamCity(String string) {
+        String tmp = string.replace("|", "||");
+        tmp = tmp.replace("'", "|'");
+        tmp = tmp.replace("\"", "|'");
+        tmp = tmp.replaceAll("\\n","|n");
+        tmp = tmp.replaceAll("\\r","|r");
+        tmp = tmp.replace("[", "|[");
+        tmp = tmp.replace("]", "|]");
+        return tmp;
+    }
+
     protected void handleLine(String line) {
         Matcher m;
         if (!ignoreUnitTests) {
             m = START_SUITE.matcher(line);
             if (m.matches()) {
-                buildListener.message("##teamcity[testSuiteStarted name='" + m.group(1) + "']");
+                buildListener.message("##teamcity[testSuiteStarted name='" + escapeForTeamCity(m.group(1)) + "']");
                 return;
             }
 
             m = END_SUITE.matcher(line);
             if (m.matches()) {
-                buildListener.message("##teamcity[testSuiteFinished name='" + m.group(1) + "']");
+                buildListener.message("##teamcity[testSuiteFinished name='" + escapeForTeamCity(m.group(1)) + "']");
                 return;
             }
 
             m = START_TESTCASE.matcher(line);
             if (m.matches()) {
-                buildListener.message("##teamcity[testStarted name='" + m.group(1) + "']");
+                buildListener.message("##teamcity[testStarted name='" + escapeForTeamCity(m.group(1)) + "']");
                 return;
             }
 
             m = END_TESTCASE.matcher(line);
             if (m.matches()) {
-                buildListener.message("##teamcity[testFinished name='" + m.group(1) + "' duration='" + m.group(2) + "']");
+                buildListener.message("##teamcity[testFinished name='" + escapeForTeamCity(m.group(1)) + "' duration='" + m.group(2) + "']");
                 return;
             }
 
             m = ERROR_TESTCASE.matcher(line);
             if (m.matches()) {
-                buildListener.message("##teamcity[testFailed name='" + m.group(3) + "' message='" + m.group(4) + "']");
+                buildListener.message("##teamcity[testFailed name='" + escapeForTeamCity(m.group(3)) + "' message='" + escapeForTeamCity(m.group(4)) + "']");
                 return;
             }
 
             m = FAILED_TESTCASE.matcher(line);
             if (m.matches()) {
-                buildListener.message("##teamcity[testFailed name='" + m.group(1) + "' duration='" + m.group(2) + "']");
+                buildListener.message("##teamcity[testFailed name='" + escapeForTeamCity(m.group(1)) + "' duration='" + escapeForTeamCity(m.group(2)) + "']");
                 return;
             }
         }
